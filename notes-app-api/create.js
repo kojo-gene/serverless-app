df167333b9ccea7e8115707fca42a839162b7176
captain-c00keys <gpieterson.wsu@hotmail.com>
2019-03-28 19:05:30 -0700

    added notes

---
+++ notes-app-api/create.js
@@ -0,0 +1,28 @@
+import uuid from "uuid";
+import * as dynamoDb from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+AWS.config.update({ region: "us-west-2" });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event, context) {
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: "notes",
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
+}
